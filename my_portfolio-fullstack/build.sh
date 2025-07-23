3a40930048d392d4827a5af124c48c4fbcc51ef4
kmedya-dev <koustavamedya.2000@gmail.com>
2025-07-23 22:55:31 +0530

    feat: Add debugging to build.sh for superuser creation

--- my_portfolio-fullstack/build.sh
+++ my_portfolio-fullstack/build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+set -x
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "Attempting to create superuser..."
+python manage.py createsuperuser --noinput
+echo "Superuser creation command finished."
