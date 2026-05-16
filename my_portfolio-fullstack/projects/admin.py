c148eef180c55fbbeb35ebe9449b85738b73bfdc
Koustava Medya <koustavamedya.2000@gmail.com>
2026-05-16 17:26:15 +0000

    feat: implement slug-based routing and modern project showcase with tailwind

--- my_portfolio-fullstack/projects/admin.py
+++ my_portfolio-fullstack/projects/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Project
 
-admin.site.register(Project)
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'created_at')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title', 'description', 'technologies')
+    list_filter = ('created_at',)
--- my_portfolio-fullstack/projects/models.py
+++ my_portfolio-fullstack/projects/models.py
@@ -1,17 +1,30 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     description = models.TextField()
-    # image = models.ImageField(upload_to='projects/', blank=True, null=True)
-    link = models.URLField(blank=True, null=True)
-    github_link = models.URLField(blank=True, null=True)
+    image = models.ImageField(upload_to='projects/', blank=True, null=True)
+    technologies = models.CharField(max_length=500, help_text="Comma-separated technologies", blank=True)
+    link = models.URLField(blank=True, null=True, help_text="Live Demo Link")
+    github_link = models.URLField(blank=True, null=True, help_text="GitHub Repository Link")
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return self.title
 
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
     def get_absolute_url(self):
-        return reverse('project_detail', kwargs={'pk': self.pk})
+        return reverse('project_detail', kwargs={'slug': self.slug})
+
+    def get_tech_list(self):
+        if self.technologies:
+            return [tech.strip() for tech in self.technologies.split(',')]
+        return []
